77082ca30adc6a531b7d87ad5cc2361aa6fc611b
Stanford S Guillory <stanford.guillory@gmail.com>
2021-05-29 22:28:27 -0500

    StanG: Fixed typo in Pipeline stack name

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from aws_cdk import core as cdk
 from cdk_trainer.pipeline_stack import PipelineStack
 
 app = cdk.App()
-PipelineStack(app, "CdkTrainerPipeeline",
+PipelineStack(app, "CdkTrainerPipeline",
               # If you don't specify 'env', this stack will be environment-agnostic.
               # Account/Region-dependent features and context lookups will not work,
               # but a single synthesized template can be deployed anywhere.
